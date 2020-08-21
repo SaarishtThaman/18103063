39a32372ac294b176fdf004a87179fa93f6af481
Saarisht Thaman <saarishtthaman@gmail.com>
2020-08-21 16:34:30 +0530

    Uploading assignment 1

---
+++ Assignment1/q4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class q4 {
+	static boolean checkAnagram(String str1, String str2) {
+		if (str1.length()!=str2.length()) {
+			return false;
+		}
+		int value=0;
+		for (int i=0;i<str1.length();i++) {
+			value=value^(int)str1.charAt(i);
+			value=value^(int)str2.charAt(i);
+		}
+		return value==0;	
+	}
+	public static void main(String[] args) {
+		String s1,s2;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter string 1:");
+		s1=sc.nextLine();
+		System.out.println("Enter string 2:");
+		s2=sc.nextLine();
+		boolean ans=checkAnagram(s1,s2);
+		if(ans==true)
+			System.out.println("Strings are anagrams of each other");
+		else
+			System.out.println("Strings are not anagrams");
+	}
+}
